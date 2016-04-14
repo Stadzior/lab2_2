8db3a0de7e2fb8a79cd372b349b812b003da5b35
Stadzior <Stadzior@gmail.com>
2016-04-14 18:26:22 +0200

    Refactoring of MoneyTest class.

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -7,16 +8,24 @@ import java.util.Currency;
  * Created by Kamil on 2016-04-14.
  */
 public class MoneyTest {
+
+    Money euros;
+    Money dollars;
+    @Before
+    public void initialize(){
+        euros = new Money(1, Currency.getInstance("EUR"));
+        dollars = new Money(1,Currency.getInstance("USD"));
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowException_WhenSubtractMissmatchedCurrencies(){
-        Money euros = new Money(1, Currency.getInstance("EUR"));
-        Money dollars = new Money(1,Currency.getInstance("USD"));
+
         euros.subtract(dollars);
     }
+
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowException_WhenAddMissmatchedCurrencies() {
-        Money euros = new Money(1, Currency.getInstance("EUR"));
-        Money dollars = new Money(1, Currency.getInstance("USD"));
+
         euros.add(dollars);
     }
 }
